43a01e0aebff081150abfded0c98d3ef4ee7bb1d
Rubal <rahmed2210@gmail.com>
2023-05-11 14:02:28 -0400

    How to add new element in javaScript

--- notesApps/notesApp.js
+++ notesApps/notesApp.js
@@ -1,33 +1,44 @@
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Bangladesh',
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise, eat a bit better',
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+
 // DOM -> Document Object Model
 
 
 // Query and remove
-/*
-const p = document.querySelector('p')
-p.remove()
-*/
 
-//-------------------------------------------------------------------------------
+// const p = document.querySelector('p')
+// p.remove()
 
-// .forEach -> allows us to iterate over each thing 
-// (Function) -> we pass in our callback functions
-// () { })  -> like for arrays - we get access to the individual item
 
 // Query all and remove
-const ps = document.querySelectorAll('p')
-ps.forEach(function (p) {
-    console.log(p.textContent)
-    p.remove()
-})
+// const ps = document.querySelectorAll('p')
+
+// ps.forEach(function (p) {
+//     console.log(p.textContent)
+//     p.remove()
+// })
 
 // Query all and rewrite
-/*  
 const ps = document.querySelectorAll('p')
 ps.forEach(function (p) {
     p.textContent = '*****'
 })
-*/
 
 // .forEach -> allows us to iterate over each thing 
 // (Function) -> we pass in our callback functions
 // () { })  -> like for arrays - we get access to the individual item
+
+// Add a new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
--- todoApps/todoApp.js
+++ todoApps/todoApp.js
@@ -1,3 +1,21 @@
+const todos = [
+    {
+      text: "Order cat food",
+      completed: false,
+    }, {
+      text: "Clean kitchen",
+      completed: true,
+    }, {
+      text: "Buy food",
+      completed: true,
+    }, {
+      text: "Do work",
+      completed: false,
+    }, {
+      text: "Exercise",
+      completed: true,
+    }]
+
 const paragraphs = document.querySelectorAll('p')
 
 // removed tag the contain 'the'

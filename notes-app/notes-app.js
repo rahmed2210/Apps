0ae42c6ed1406eb2348022684bc153c085c38bcb
Rubal <rahmed2210@gmail.com>
2024-10-25 21:44:00 -0400

    Rendering our Filtered Data

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,10 +9,35 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+// Rendering Our Filtered Data
 
+// setting up a filters objects to store the latest filters
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    // Duplicate Notes (filtering) --clear div entirely when adding new notes
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
+
+// ---
 // User Interaction
 document.querySelector('#create-note').addEventListener('click', function (e) {
-    e.target.textContent = 'The button was click'
+    e.target.textContent = 'The button was clicked'
 })
 
 // ID and Class elements
@@ -22,13 +47,30 @@ document.querySelector('#remove-all').addEventListener('click', function () {
     })
 })
 
+// text Input and Data Filtering
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
 
-//text Input and Data Filtering
+
+
+/*
+// User Interaction
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    e.target.textContent = 'The button was clicked'
+})
+
+// ID and Class elements
+document.querySelector('#remove-all').addEventListener('click', function () {
+    document.querySelectorAll('.note').forEach(function( note) {
+        note.remove()
+    })
+})
+
+// text Input and Data Filtering
 document.querySelector('#search-text').addEventListener('input', function (e) {
     console.log(e.target.value)
 })
 
-// INPUT --> event is going to fire on every single character change
-
-// using INPUT event with text based inputs, allowing to access that latest information
-
+*/

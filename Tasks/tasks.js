3cc157c84e3ab0d6d3f89115f59c7bb9d8c1dec5
Rubal <rahmed2210@gmail.com>
2024-10-30 05:23:40 -0400

    CheckBox

--- Tasks/tasks.js
+++ Tasks/tasks.js
@@ -68,26 +68,3 @@ document.querySelector('#hide-completed').addEventListener('change', function (e
     renderTodos(todos, filters)
 })
 
-
-
-
-/**
- 
- const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed
-    })
-
-    document.querySelector('#todos').innerHTML = ''
-
-    const summary = document.createElement('h2')
-    summary.textContent = `You have ${incompleteTodos.length} todos left`
-    document.querySelector('#todos').appendChild(summary)
-
-    filteredTodos.forEach(function (todo) {
-        const p = document.createElement('p')
-        p.textContent = todo.text
-        document.querySelector('#todos').appendChild(p)
-    })
-
-
- */

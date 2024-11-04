1df6dd26acb73c51d4fd001ae7e4c127fcb4b59f
Rubal <rahmed2210@gmail.com>
2024-11-04 17:37:29 -0500

    remove Button

--- Tasks/tasks-functions.js
+++ Tasks/tasks-functions.js
@@ -35,11 +35,28 @@ const renderTodos = function (todos, filters) {
     })
 }
 
-// Get the DOM elements for an individual note
+// Get the DOM elements for an individual note 
 const generateTodoDOM = function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    return p
+    const todoEl = document.createElement('div')
+    const checkBox = document.createElement('input')
+    const todoText = document.createElement('span')
+    const removeButton = document.createElement('button')
+
+ //   const removeButton = document.createElement('button')
+
+    // Setup todo checkBox
+    checkBox.setAttribute('type', 'checkbox')
+    todoEl.appendChild(checkBox)
+
+    // setup the todo text
+    todoText.textContent = todo.text
+    todoEl.appendChild(todoText)
+
+    // setup the remove button
+    removeButton.textContent = 'x'
+    todoEl.appendChild(removeButton)
+
+    return todoEl
 }
 
 // Get the DOM elements for list summary

6f55202599369deb35227809a96b75d852dde228
Rubal <rahmed2210@gmail.com>
2024-11-04 17:36:07 -0500

    remove Button

---
+++ apps-Lecture/N11_removeButton-app.js
@@ -0,0 +1,32 @@
+const notes = getSavedNotes()
+
+// Rendering Our Filtered Data
+const filters = {
+    searchText: ''
+}
+
+renderNotes(notes, filters)
+
+// User Interaction
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    // want to push the new item on to the notes array
+    notes.push({
+        title: '',
+        body: ''
+    }) 
+    saveNotes(notes)
+    // rerender things
+    renderNotes(notes, filters)
+
+})
+
+// text Input and Data Filtering
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+// Dropdowns menu
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})

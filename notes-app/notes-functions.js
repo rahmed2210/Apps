ef5fd6ea5d3e7d43eed9680c5bbc2461781469e6
Rubal <rahmed2210@gmail.com>
2024-11-06 21:28:30 -0500

    Targeting by UUID

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -1,5 +1,3 @@
-
-
 // Reading existing notes from localStorage
 const getSavedNotes = function () {
     const notesJSON = localStorage.getItem('notes')
@@ -17,6 +15,17 @@ const saveNotes = function (notes) {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
+// Remove a note from the list
+const removeNote = function (id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id === id
+    })
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+}
+
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
     const noteEl = document.createElement('div')
@@ -26,6 +35,11 @@ const generateNoteDOM = function (note) {
     // Setup the remove note button
     button.textContent = 'x'
     noteEl.appendChild(button)
+    button.addEventListener('click', function () {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
 
     // Setup the note title text
     if (note.title.length > 0) {
@@ -46,7 +60,7 @@ const renderNotes = function (notes, filters) {
 
     // Duplicate Notes (filtering) --clear div entirely when adding new notes
     document.querySelector('#notes').innerHTML = ''
-    
+
     filteredNotes.forEach(function (note) {
         const noteEl = generateNoteDOM(note)
         document.querySelector('#notes').appendChild(noteEl)

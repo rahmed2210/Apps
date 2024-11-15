331bef4eb83065a9778346b2098c42366bab6758
Rubal <rahmed2210@gmail.com>
2024-11-15 14:56:26 -0500

    Syncing Data Across Page

---
+++ apps-Lecture/N15_SyncingData-editnotes-app.js
@@ -0,0 +1,42 @@
+let notes = getSavedNotes()
+
+// Rendering Our Filtered Data
+const filters = {
+    searchText: ''
+}
+
+renderNotes(notes, filters)
+
+// User Interaction
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    const id = uuidv4()
+    // want to push the new item on to the notes array
+    notes.push({
+        id: id,
+        title: '',
+        body: ''
+    }) 
+    saveNotes(notes)
+    location.assign(`./edit.html#${id}`)
+
+})
+
+// text Input and Data Filtering
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+// Dropdowns menu
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
+
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        // 1. Parse the new data and update notes
+        notes = JSON.parse(e.value)
+        // 2. rerender the notes
+        renderNotes(notes, filters)
+    }
+})
